233fd18a9fafef03faad2960a5cd0ba4030ef7cb
Lucas <luccaspms@hotmail.com>
2025-08-13 23:34:02 -0300

    Primeiro commit

---
+++ service-worker.js.js
@@ -0,0 +1,45 @@
+// service-worker.js
+const CACHE_NAME = 'diagnostico-rural-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se encontrado, senão busca na rede
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualizar cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
